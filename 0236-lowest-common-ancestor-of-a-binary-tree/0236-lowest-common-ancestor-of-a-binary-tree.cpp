a9f8c1ec305ab35275b293d38dda394caaf93293
Sameer Jha <90266216+Sameerjha236@users.noreply.github.com>
2023-06-07 11:22:36 +0530

    Time: 19 ms (55.99%), Space: 14.2 MB (85.06%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,52 +9,13 @@
  */
 class Solution {
 public:
-
-    void backtrack(TreeNode * root,TreeNode* curr, bool &flag, vector<TreeNode *> & store)
-    {
-        if(root==nullptr)
-            return;
-        if(root->val == curr->val)
-        {
-            flag=1;
-            store.push_back(root);
-            return;
-        }
-        if(flag==1)
-        {
-            store.push_back(root);
-            return;
-        }
-        backtrack(root->left,curr,flag,store);
-        if(flag==1)
-        {
-            store.push_back(root);
-            return;
-        }
-        backtrack(root->right,curr,flag,store);
-        if(flag==1)
-        {
-            store.push_back(root);
-            return;
-        }
-        
-    }
-
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode *> first;
-        vector<TreeNode *> second;
-        bool f=0;
-        backtrack(root,p,f,first);
-        f=0;
-        backtrack(root,q,f,second);
-        for(auto i:first)
-        {
-            for(auto j:second)
-            {
-                if(i==j)
-                    return i;
-            }
-        }
-        return root;
+        if(root!=NULL && (root==p || root==q)) return root;
+        if(root==NULL) return root;
+        TreeNode* l = lowestCommonAncestor(root->left,p,q);
+        TreeNode* r = lowestCommonAncestor(root->right,p,q);
+        if(!l) return r ;
+       else if(!r) return l;
+       else return root;
     }
 };
