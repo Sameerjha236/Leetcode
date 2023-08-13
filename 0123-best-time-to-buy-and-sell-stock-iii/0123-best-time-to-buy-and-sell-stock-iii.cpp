a57bcbeada9b6c7ecff39380ab204aac46b8fdd1
Sameer Jha <90266216+Sameerjha236@users.noreply.github.com>
2023-08-14 01:28:34 +0530

    Time: 493 ms (58.51%), Space: 207 MB (47.27%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,37 +1,72 @@
-class Solution {
-public:
 
-    int solve(int i, int buy, int trans, vector<int> &prices,vector<vector<vector<int> > > &dp)
+class Solution
+{
+public:
+    int solve(int i, int buy, int trans, vector<int> &prices, vector<vector<vector<int>>> &dp)
     {
-        if(i == prices.size()) return 0;
-        if(trans == 0) return 0;
-        if(dp[i][buy][trans] !=-1) return dp[i][buy][trans];
+        if (i == prices.size())
+            return 0;
+        if (trans == 0)
+            return 0;
+        if (dp[i][buy][trans] != -1)
+            return dp[i][buy][trans];
         int profit = 0;
 
-        if(buy)
+        if (buy)
         {
-            int a = -prices[i] + solve(i+1,0,trans,prices,dp);
+            int a = -prices[i] + solve(i + 1, 0, trans, prices, dp);
 
-            int b = solve(i+1,1,trans,prices,dp);
+            int b = solve(i + 1, 1, trans, prices, dp);
 
-            profit = max(a,b);
+            profit = max(a, b);
         }
         else
         {
-            int a = prices[i] + solve(i+1,1,trans-1,prices,dp);
+            int a = prices[i] + solve(i + 1, 1, trans - 1, prices, dp);
 
-            int b = solve(i+1,0,trans,prices,dp);
+            int b = solve(i + 1, 0, trans, prices, dp);
 
-            profit = max(a,b);
+            profit = max(a, b);
         }
 
         return dp[i][buy][trans] = profit;
     }
 
-    int maxProfit(vector<int>& prices) {
+    int maxProfit(vector<int> &prices)
+    {
         int n = prices.size();
-        vector<vector<vector<int> > > dp(
-        n, vector<vector<int> >(2, vector<int>(3,-1)));
-        return solve(0,1,2,prices,dp);
+        vector<vector<vector<int>>> dp(
+            n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int buy = 0; buy <= 1; buy++)
+            {
+                for (int trans = 1; trans <= 2; trans++)
+                {
+                    int profit = 0;
+
+                    if (buy)
+                    {
+                        int a = -prices[i] + dp[i + 1][0][trans];
+
+                        int b = dp[i + 1][1][trans];
+
+                        profit = max(a, b);
+                    }
+                    else
+                    {
+                        int a = prices[i] + dp[i + 1][1][ trans - 1];
+
+                        int b = dp[i + 1][0][ trans];
+
+                        profit = max(a, b);
+                    }
+                    dp[i][buy][trans] = profit;
+                }
+            }
+        }
+
+        return dp[0][1][2];
     }
 };
