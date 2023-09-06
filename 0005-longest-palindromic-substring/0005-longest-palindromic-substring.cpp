1ad1903de01d3b71860c97cf9034f63eef5696a4
Sameer Jha <90266216+Sameerjha236@users.noreply.github.com>
2023-09-07 01:01:33 +0530

    Time: 13 ms (81.25%), Space: 6.9 MB (76.35%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,47 +1,32 @@
 class Solution {
 public:
+
+    int helper(string &s,int l, int r)
+    {
+        while(l>=0 && r<s.size() && s[l] == s[r])
+        {
+            l--;
+            r++;
+        }
+        return r-l-1;
+    }
+
     string longestPalindrome(string s) {
-        int n=s.size(),ans=1;
-        bool dp[n][n];
-        memset(dp,0,sizeof(dp));
-        // i index denotes start and j denotes end index
-        for(int i=0;i<n;i++)
+        string ans;
+        int len = 1,st=0;
+        for(int i=0;i<s.size() - 1; i++)
         {
-            dp[i][i]=1;
-        }   
-        int start = 0;
-        for(int i=0;i<n-1;i++)
-        {
-            if(s[i]==s[i+1])
+            int odd = helper(s,i,i);
+            int even = helper(s,i,i+1);
+            int curr = max(odd,even);
+
+            if(curr>len)
             {
-                dp[i][i+1]=1;
-                ans = 2;
-                start = i;
+                len = curr;
+                st = i - (curr-1)/2;
             }
         }
-
-        for(int k=3;k<=n;k++)
-        {
-            for(int i=0;i<n-k+1;i++)
-            {
-                int j= i+k-1;
-
-                if(s[i] == s[j] && dp[i+1][j-1])
-                {
-                    dp[i][j]=1;
-                    if(k>ans)
-                    {
-                        ans=k;
-                        start=i;
-                    }
-                }
-            }
-        }
-        string t;
-        for(int i=start;i<start+ans;i++)
-        {
-            t+=s[i];
-        }
-        return t;
+        ans = s.substr(st,len);
+        return ans;
     }
 };
