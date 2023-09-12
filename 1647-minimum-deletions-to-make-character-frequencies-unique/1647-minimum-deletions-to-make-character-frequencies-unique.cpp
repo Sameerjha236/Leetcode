d1850e4f1a20aa67abd96971442d8f6f384c9843
Sameer Jha <90266216+Sameerjha236@users.noreply.github.com>
2023-09-13 00:53:01 +0530

    Time: 54 ms (65.33%), Space: 17.4 MB (37.86%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,20 +1,28 @@
+
 class Solution {
 public:
     int minDeletions(string s) {
         int ans = 0;
     vector<int> count(26);
-    unordered_set<int> usedFreq;
+    unordered_set<int> st;
 
     for (const char c : s)
       ++count[c - 'a'];
 
     for (int freq : count)
-      while (freq > 0 && !usedFreq.insert(freq).second) 
-      {
-        --freq;  // Delete ('a' + i).
-        ++ans;
-      }
+    {
+        while(freq>0)
+        {
+          if(st.find(freq) == st.end())
+          {
+            st.insert(freq);
+            break;
+          }
+          freq--;
+          ans++;
+        }
+    }
 
-    return ans;
+    return ans; 
     }
 };
