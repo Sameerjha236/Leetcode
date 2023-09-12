c0b62a6acf3f5cc48838d9f8bc52c7302855dd2f
Sameer Jha <90266216+Sameerjha236@users.noreply.github.com>
2023-09-13 00:44:32 +0530

    Time: 52 ms (71.58%), Space: 17.4 MB (58.00%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        if(!s.size()) return 0;
-        
-        unordered_map<char,int> chac;
-        for(auto i:s) chac[i]++;
-
-        map<int,int> num;
-        for(auto k:chac)
-        {
-            num[k.second] ++;
-        }
         int ans = 0;
-        for(auto f:num)
-        {
-            int val = f.first, freq = f.second;
-            for(int i=freq; i>1;i--)
-            {
-                for(int j=val;j>=0;j--)
-                {
-                    if(j == 0) ans += val;
-                    else if(num[j]==0)
-                    {
-                        ans += val - j;
-                        num[j]++;
-                        break;
-                    }
+    vector<int> count(26);
+    unordered_set<int> usedFreq;
 
-                }
-            }
-        }
-        sort(s.begin(),s.end());
-        cout<<s<<endl;
-        return ans;
+    for (const char c : s)
+      ++count[c - 'a'];
+
+    for (int freq : count)
+      while (freq > 0 && !usedFreq.insert(freq).second) 
+      {
+        --freq;  // Delete ('a' + i).
+        ++ans;
+      }
+
+    return ans;
     }
 };
