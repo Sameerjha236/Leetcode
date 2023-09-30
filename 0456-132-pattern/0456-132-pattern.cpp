a7d96c323fd117e377d9ae5cd14715b3d10f0300
Sameer Jha <90266216+Sameerjha236@users.noreply.github.com>
2023-10-01 01:17:45 +0530

    Time: 58 ms (93.21%), Space: 48.9 MB (39.00%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.cpp
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
-        if(nums.size()<=2) return 0;
-        stack<pair<int,int>> store;
-        int currMin = nums[0], n=nums.size();
-        
-        for(int i=1;i<n;i++)
-        {
-            int j = nums[i];
-            while(!store.empty() && store.top().first <= j)
-                store.pop();
-            
-            if(!store.empty() && j > store.top().second)
-                return 1;
-            store.push(make_pair(j,currMin));
-            currMin = min(currMin, j);
-        }
+        stack<int> store;
+        int sdMax = INT_MIN, n = nums.size();
 
+        for(int i=n-1;i>=0;i--)
+        {
+            int k = nums[i];
+            if(k<sdMax) return true;
+            while(!store.empty() && store.top()<k)
+            {
+                sdMax = store.top();
+                store.pop(); 
+            }
+            store.push(k);
+        }
         return 0;
     }
 };
