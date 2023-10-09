64034d989e4a0d6e89bbbe7c5d1e07028d720d10
Sameer Jha <90266216+Sameerjha236@users.noreply.github.com>
2023-10-10 01:06:23 +0530

    Time: 131 ms (80.74%), Space: 83 MB (57.52%) - LeetHub

--- 1471-the-k-strongest-values-in-an-array/1471-the-k-strongest-values-in-an-array.cpp
+++ 1471-the-k-strongest-values-in-an-array/1471-the-k-strongest-values-in-an-array.cpp
@@ -5,13 +5,13 @@ public:
         vector<int> ans;
         sort(arr.begin(), arr.end());
         int a = arr.size(), m = arr[(a-1)/2], i=0, j=a-1;
-        while(i<=j)
+        while(i<=j && k--)
         {
             if(abs(arr[i]-m) > abs(arr[j]-m))
                 ans.push_back(arr[i++]);
             else
                 ans.push_back(arr[j--]);
         }
-        return {ans.begin(), ans.begin()+k};
+        return ans;
     }
 };
