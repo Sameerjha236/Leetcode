13b235e8f5a2a10629381508d8dbe2bc5731577a
Sameer Jha <90266216+Sameerjha236@users.noreply.github.com>
2023-10-28 01:24:43 +0530

    Time: 126 ms (58.05%), Space: 87.6 MB (82.20%) - LeetHub

--- 2874-maximum-value-of-an-ordered-triplet-ii/2874-maximum-value-of-an-ordered-triplet-ii.cpp
+++ 2874-maximum-value-of-an-ordered-triplet-ii/2874-maximum-value-of-an-ordered-triplet-ii.cpp
@@ -2,24 +2,29 @@ class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
         int n = nums.size();
-        if(n<=2) return 0;
-        vector<int> pre(n,-1);
-        vector<int> suf(n,-1);
-        
-        pre[0] = nums[0];
-        for(int i=1;i<n;i++) pre[i] = max(nums[i],pre[i-1]);
-        
-        suf[n-1] = nums[n-1];
-        for(int i=n-2;i>=0;i--) suf[i] = max(nums[i],suf[i+1]);
-        long long ans = 0;
-        
-        for(int j=1;j<n-1;j++)
+        int pre[n], suff[n], curr = 0;
+        for(int i=0;i<n;i++)
         {
-            long long k = pre[j-1] - nums[j];
-            long long curr = k * suf[j+1];
-            if(curr>ans) ans = curr; 
+            pre[i] = curr;
+            curr = max(curr,nums[i]);
+        }
+        curr = nums[n-1];
+        suff[n-1] = 0;
+        for(int i=n-2;i>=0;i--)
+        {
+            suff[i] = curr;
+            curr = max(curr,nums[i]);
+        }
+
+        long long ans;
+        for(int i=1;i<n-1;i++)
+        {
+            int a = pre[i];
+            int b = suff[i];
+            long long int c = (a - nums[i]);
+            c *= b;
+            ans = max(c,ans);
         }
         return ans;
-
     }
 };
