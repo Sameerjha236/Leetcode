4fa574100839daaaed00e1d94506ce322ca939ca
Sameer Jha <90266216+Sameerjha236@users.noreply.github.com>
2023-10-30 23:49:52 +0530

    Added solution - LeetHub

--- Eventual Safe States - GFG/eventual-safe-states.cpp
+++ Eventual Safe States - GFG/eventual-safe-states.cpp
@@ -9,41 +9,44 @@ using namespace std;
 // User function Template for C++
 
 class Solution {
-  public:
-  
-    bool dfs(int node, vector<bool> &vis, vector<bool> &path, vector<bool> &check, vector<int> adj[])
+    
+    bool dfs(int i, vector<int> &vis, vector<int> &path, vector<int> adj[])
     {
-        vis[node] = 1;
-        path[node] = 1;
-        
-        for(auto k:adj[node])
+        vis[i] = 0;
+        path[i] = 1;
+        for(auto k: adj[i])
         {
-            if(vis[k] && path[k]) return true;
-            else if(vis[k]) continue;
-            bool c= dfs(k,vis,path,check,adj);
-            if(c) return true; 
-        }
-        check[node] = 1;
-        path[node] = 0;
-        return false;
-    }
-  
-    vector<int> eventualSafeNodes(int n, vector<int> adj[]) {
-        vector<bool> vis(n,0);
-        vector<bool> path(n,0);
-        vector<bool> check(n,0);
-
-        for(int i=0;i<n;i++)
-        {
-            if(!vis[i])
+            if(path[k])
+                return 0;
+            else if(vis[k] == 0) 
             {
-                dfs(i,vis,path,check,adj);
+                return 0;
             }
+            bool ck = dfs(k,vis,path,adj);
+            if(ck == 0) return 0;
+        }
+        vis[i] = 1;
+        path[i] = 0;
+        return 1;
+    }
+    
+  public:
+    vector<int> eventualSafeNodes(int v, vector<int> adj[]) {
+        vector<int> vis(v,-1);
+        vector<int> path(v,0);
+        for(int i=0;i<v;i++)
+        {
+            if(adj[i].size() == 0) vis[i] = 1;
+        }
+        for(int i=0;i<v;i++)
+        {
+            if(vis[i] > -1) continue;
+            dfs(i,vis,path,adj);
         }
         vector<int> ans;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<v;i++)
         {
-            if(check[i]) ans.push_back(i);
+            if(vis[i]) ans.push_back(i);
         }
         return ans;
     }
