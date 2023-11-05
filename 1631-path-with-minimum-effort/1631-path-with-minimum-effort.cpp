897d0ad33424b3f82c303a1c0eaae1cff9c99fa8
Sameer Jha <90266216+Sameerjha236@users.noreply.github.com>
2023-11-05 23:27:37 +0530

    Time: 391 ms (11.64%), Space: 51.4 MB (9.76%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,54 +1,53 @@
+class Solution {
 
-class Solution
-{
-private:
-    bool visited[105][105];
-    int directions_x[4] = {0, 1, -1, 0};
-    int directions_y[4] = {1, 0, 0, -1};
-    int numRows, numCols;
-
-public:
-    void dfs(int x, int y, int limitEffort, vector<vector<int>> &heights)
+    bool check(int k, vector<vector<int>> &grid)
     {
-        if (visited[x][y])
-            return;
-        visited[x][y] = true;
+        int m = grid.size(), n = grid[0].size();
+        
+        vector<pair<int,int>> cord = {{0,1},{0,-1},{-1,0},{1,0}};
+        vector<vector<bool>> vis(m, vector<bool> (n,1));
+        queue<pair<int,int>> q;
+        
+        vis[0][0] = 0;
+        q.push({0,0});
 
-        if (x == numRows - 1 && y == numCols - 1)
-            return;
-        for (int i = 0; i < 4; i++)
+        while(!q.empty())
         {
-            int new_x = x + directions_x[i];
-            int new_y = y + directions_y[i];
+            int r = q.front().first;
+            int c = q.front().second;
+            if(r == m-1 && c == n-1) return 1;
+            q.pop();
+            for(auto z: cord)
+            {
+                int x = r + z.first;
+                int y = c + z.second;
+                if(x>=0 && y>=0 && x<m && y<n && vis[x][y] && abs(grid[x][y] - grid[r][c]) <= k)
+                {
 
-            if (new_x < 0 || new_x >= numRows || new_y < 0 || new_y >= numCols)
-                continue;
-
-            int newEffort = abs(heights[x][y] - heights[new_x][new_y]);
-            if (newEffort <= limitEffort)
-                dfs(new_x, new_y, limitEffort, heights);
+                    vis[x][y] = 0;
+                    q.push({x,y});
+                }
+            }
         }
+        return 0;
     }
 
-    int minimumEffortPath(vector<vector<int>> &heights)
-    {
-        numRows = heights.size();
-        numCols = heights[0].size();
-
-        int lowerLimit = 0, upperLimit = 1e6;
-
-        while (lowerLimit < upperLimit)
+public:
+    int minimumEffortPath(vector<vector<int>>& grid) {
+        int ans = 0, l = 0, r = 1e6;
+        while(l<=r)
         {
-            int mid = (upperLimit + lowerLimit) / 2;
-            memset(visited, 0, sizeof visited);
-            dfs(0, 0, mid, heights);
-
-            if (visited[numRows - 1][numCols - 1])
-                upperLimit = mid;
+            int m = (l+r)/2;
+            if(check(m,grid))
+            {
+                ans = m;
+                r = m-1;
+            } 
             else
-                lowerLimit = mid + 1;
+            {
+                l = m+1;
+            } 
         }
-
-        return lowerLimit;
+        return ans;
     }
 };
