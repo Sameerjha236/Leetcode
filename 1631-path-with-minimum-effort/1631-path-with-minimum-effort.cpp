7e559a5b9f28476624e47b3f36645abb7f19e813
Sameer Jha <90266216+Sameerjha236@users.noreply.github.com>
2023-11-06 02:38:33 +0530

    Time: 897 ms (5.00%), Space: 121.1 MB (5.01%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,53 +1,40 @@
 class Solution {
-
-    bool check(int k, vector<vector<int>> &grid)
-    {
+public:
+    int minimumEffortPath(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
         
-        vector<pair<int,int>> cord = {{0,1},{0,-1},{-1,0},{1,0}};
-        vector<vector<bool>> vis(m, vector<bool> (n,1));
-        queue<pair<int,int>> q;
+        vector<pair<int,int>> cord = {{0,1},{0,-1}, {1,0}, {-1,0}};
+        queue<pair<int, pair<int,int>>> q;
+        vector<vector<int>> dist(m, vector<int> (n,INT_MAX));
         
-        vis[0][0] = 0;
-        q.push({0,0});
-
+        dist[0][0] = 0;
+        q.push({0, {0,0}});
+        
         while(!q.empty())
         {
-            int r = q.front().first;
-            int c = q.front().second;
-            if(r == m-1 && c == n-1) return 1;
+            int cost = q.front().first;
+            int r = q.front().second.first;
+            int c = q.front().second.second;
             q.pop();
-            for(auto z: cord)
+            
+            for(auto k: cord)
             {
-                int x = r + z.first;
-                int y = c + z.second;
-                if(x>=0 && y>=0 && x<m && y<n && vis[x][y] && abs(grid[x][y] - grid[r][c]) <= k)
+                int x = r + k.first;
+                int y = c + k.second;
+                
+                if(x>=0 && y>=0 && x<m && y<n)
                 {
-
-                    vis[x][y] = 0;
-                    q.push({x,y});
+                    int val = abs(grid[x][y] - grid[r][c]);
+                    val = max(val, cost);
+                    
+                    if(val < dist[x][y])
+                    {
+                        dist[x][y] = val;
+                        q.push({val, {x,y}});
+                    }
                 }
             }
         }
-        return 0;
-    }
-
-public:
-    int minimumEffortPath(vector<vector<int>>& grid) {
-        int ans = 0, l = 0, r = 1e6;
-        while(l<=r)
-        {
-            int m = (l+r)/2;
-            if(check(m,grid))
-            {
-                ans = m;
-                r = m-1;
-            } 
-            else
-            {
-                l = m+1;
-            } 
-        }
-        return ans;
+        return dist[m-1][n-1];
     }
 };
