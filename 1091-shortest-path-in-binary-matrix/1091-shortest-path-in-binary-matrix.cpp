7f2d1542d9829cc31ba3d8fef1b4e9d428d592cd
Sameer Jha <90266216+Sameerjha236@users.noreply.github.com>
2023-11-06 02:20:07 +0530

    Time: 60 ms (47.50%), Space: 22.7 MB (46.14%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int dr[]={0,0,-1,1,-1,1,-1,1};
-        int dc[]={-1,1,0,0,-1,1,1,-1};
-        if(grid[0][0]==1)
-        {
-            return -1;
-        }
-        queue<pair<int,pair<int,int>>> q;
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>> dis(n,vector<int>(m,1e9));
-        dis[0][0]=0;
-        q.push({0,{0,0}});
+        int n = grid[0].size();
+        if(grid[0][0] || grid[n-1][n-1]) return -1;
+        vector<pair<int,int>> cord = {{0,1}, {0,-1}, {1,0}, {-1,0} ,{1,1}, {1,-1},{-1,1},{-1,-1}};
+        vector<vector<int>> dist(n, vector<int> (n,INT_MAX));
+        queue<pair<int, pair<int,int>>> q;
+        
+        dist[0][0] = 1;
+        q.push({1, {0, 0}});
+        
         while(!q.empty())
         {
-            int x=q.front().second.first;
-            int y=q.front().second.second;
-            int cost=q.front().first;
-            if(x==n-1 && y==m-1)
-            {
-                return cost+1;
-            }
+            int cost = q.front().first;
+            int x = q.front().second.first;
+            int y = q.front().second.second;
             q.pop();
-            for(int i=0;i<8;i++)
+            if(x == n-1 && y == n-1) return cost;
+            
+            for(auto k: cord)
             {
-                int r=x+dr[i];
-                int c=y+dc[i];
-                if(r>=0 && c>=0 && r<n && c<n && dis[r][c]>cost+1 && grid[r][c]==0)
+                int a = x + k.first;
+                int b = y + k.second;
+                
+                if(a>=0 && b>=0 && a<n && b<n && grid[a][b]==0 && dist[a][b] > cost + 1)
                 {
-                    dis[r][c]=cost+1;
-                    q.push({cost+1,{r,c}});
+                    dist[a][b] = cost + 1;
+                    q.push({cost+1, {a,b}});
                 }
             }
         }
