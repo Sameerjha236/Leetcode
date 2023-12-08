461b1fb1e9fcfc81c743abefd48ef9ce08258aad
Sameer Jha <90266216+Sameerjha236@users.noreply.github.com>
2023-12-09 02:23:25 +0530

    Time: 13 ms (73.75%), Space: 9.8 MB (60.45%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,32 +1,44 @@
 class Solution {
 public:
+    string longestPalindrome(string s) {
+        int ans = 0, n = s.size();
+        string res;
+        for(int i=0;i<=n-1;i++)
+        {
+            int a = solve(i,i,s);//odd
+            int p = (a-1)*2 + 1;
+            int b = solve(i,i+1,s);//even
+            int q = b*2;
 
-    int helper(string &s,int l, int r)
+
+            if(p>=q && p>ans)
+            {
+                res = s.substr(i - a + 1,p);
+
+                ans = p;
+            }
+            else if(q>ans)
+            {
+                res = s.substr(i-b+1,q);
+
+                ans = q;
+            }
+
+        }
+
+        return res;
+    }
+
+    int solve(int l, int r, string &s)
     {
-        while(l>=0 && r<s.size() && s[l] == s[r])
+        int n = s.size();
+        int k = 0;
+        while(l>=0 && r<n && s[l] == s[r])
         {
             l--;
             r++;
+            k++;
         }
-        return r-l-1;
-    }
-
-    string longestPalindrome(string s) {
-        string ans;
-        int len = 1,st=0;
-        for(int i=0;i<s.size() - 1; i++)
-        {
-            int odd = helper(s,i,i);
-            int even = helper(s,i,i+1);
-            int curr = max(odd,even);
-
-            if(curr>len)
-            {
-                len = curr;
-                st = i - (curr-1)/2;
-            }
-        }
-        ans = s.substr(st,len);
-        return ans;
+        return k;
     }
 };
