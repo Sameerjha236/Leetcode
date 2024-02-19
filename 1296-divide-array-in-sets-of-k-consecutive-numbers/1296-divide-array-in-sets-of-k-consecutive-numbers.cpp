1bec04f62f64790dac53078c1bbb510c354859ad
Sameer Jha <90266216+Sameerjha236@users.noreply.github.com>
2024-02-19 16:13:23 +0530

    Time: 164 ms (36.52%), Space: 53.4 MB (51.25%) - LeetHub

---
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPossibleDivide(vector<int>& hand, int g) {
+        int n = hand.size();
+        map<int,int> hash;
+        for(auto i: hand) hash[i]++;
+
+        for(auto curr:hash)
+        {
+            int a = curr.first, freq = curr.second;
+            while(freq)
+            {
+                int s = 0, k = a;
+                while(s != g)
+                {
+                    if(hash[k] == 0)
+                        return 0;
+                    n--;
+                    hash[k] = hash[k] - 1;
+                    k++;
+                    s++;
+                }
+                freq = hash[a];
+            }
+        }
+
+        return n == 0;
+    }
+};
