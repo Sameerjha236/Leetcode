3dcdc3cc2fad771ab9777303e2b57c5a030d1234
Sameer Jha <90266216+Sameerjha236@users.noreply.github.com>
2024-03-03 15:55:48 +0530

    Time: 4 ms (31.91%), Space: 9.2 MB (62.22%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,30 +1,26 @@
 class Solution {
-    int helper(int i, int chk, int dp[][2], vector<int>& nums) {
-        int n = nums.size(), tk = 0, nt = 0;
-        if (i >= n)
-            return 0;
-        if (dp[i][chk] != -1)
-            return dp[i][chk];
-        if (i == 0) {
-            tk = helper(i + 2, 1, dp, nums) + nums[i];
-            nt = helper(i + 1, 0, dp, nums);
-        } else if (i == n - 1) {
-            if (chk)
-                return 0;
-            return nums[i];
-        } else {
-            tk = nums[i] + helper(i + 2, chk, dp, nums);
-            nt = helper(i + 1, chk, dp, nums);
-        }
-        return dp[i][chk] = max(tk, nt);
-    }
-
 public:
     int rob(vector<int>& nums) {
         int ans = 0, n = nums.size();
-        int dp[n][2];
-        memset(dp, -1, sizeof(dp));
-        ans = helper(0, 0, dp, nums);
-        return ans;
+        int dp[n + 1][2];
+        memset(dp, 0, sizeof(dp));
+
+        dp[n - 1][0] = nums[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int chk = 0; chk <= 1; chk++) {
+                int tk = 0, nt = 0;
+                if (i == 0) {
+                    tk = dp[i + 2][1] + nums[i];
+                    nt = dp[i + 1][0];
+                } else {
+                    tk = nums[i] + dp[i + 2][chk];
+                    nt = dp[i + 1][chk];
+                }
+                dp[i][chk] = max(tk, nt);
+            }
+        }
+
+        return max(dp[0][0], dp[0][1]);
     }
 };
