2aa32966081182b89112efe345026cf36686b1e8
Sameer Jha <90266216+Sameerjha236@users.noreply.github.com>
2024-04-22 22:52:01 +0530

    Time: 200 ms (42.41%), Space: 67.7 MB (30.08%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.cpp
+++ 0752-open-the-lock/0752-open-the-lock.cpp
@@ -3,19 +3,6 @@ class Solution {
     vector<string> change = {"19", "02", "13", "24", "35",
                              "46", "57", "68", "79", "80"};
 
-    vector<string> helper(string curr) {
-        vector<string> add;
-        for (int i = 0; i < curr.size(); i++) {
-            int x = curr[i] - '0';
-            for (auto c : change[x]) {
-                string ch = curr;
-                ch[i] = c;
-                add.push_back(ch);
-            }
-        }
-        return add;
-    }
-
 public:
     int openLock(vector<string>& deadends, string target) {
         unordered_map<string, bool> vis, end;
@@ -35,14 +22,20 @@ public:
             for (int i = 0; i < n; i++) {
                 string curr = q.front();
                 q.pop();
+                
                 if (curr == target)
                     return ans;
-                vector<string> add = helper(curr);
-                for (auto k : add) {
-                    if (vis[k] || end[k])
-                        continue;
-                    vis[k] = 1;
-                    q.push(k);
+
+                for (int i = 0; i < curr.size(); i++) {
+                    int x = curr[i] - '0';
+                    for (auto c : change[x]) {
+                        string ch = curr;
+                        ch[i] = c;
+                        if (vis[ch] || end[ch])
+                            continue;
+                        vis[ch] = 1;
+                        q.push(ch);
+                    }
                 }
             }
             ans++;
